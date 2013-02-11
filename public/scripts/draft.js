49de759d4acf2cc5ebfad96a1c9e9dd6a9890761
Jeff Sacks <jrsacks@gmail.com>
2013-02-10 18:17:18 -0600

    improving draft layout

--- public/scripts/draft.js
+++ public/scripts/draft.js
@@ -1,4 +1,11 @@
 var colors = ["Red", "Maroon", "Yellow", "Olive","Lime","Green","Aqua","Teal","Blue","Navy","Fuchsia","Purple"];
+var playerData = {};
+
+function idFromName(name) {
+  return _.find(_.keys(playerData), function(playerId){
+    return playerText(playerData[playerId]) === name;
+  });
+}
 
 function playerText(player){
   return player.name + ' (' + player.team + ')';
@@ -13,7 +20,7 @@ function twoDigit(val){
 
 function addRowsToTeamTable() {
   _.times(10, function(i){
-    var row = $('<tr>').append($('<td>').text(i + 1));
+    var row = $('<tr>');
     _.times(12, function(i){
       row.append($('<td>'));
     });
@@ -30,14 +37,7 @@ function send(socket, msg) {
   }));
 }
 
-function handleMessage(msg) { 
-  var parsed = JSON.parse(msg.data);
-  var msgDate = new Date(parsed.timestamp);
-  $('.chat').append($('<div>')
-    .append($('<span>').addClass('chat-timestamp').text(msgDate.getHours() % 12 + ':' + twoDigit(msgDate.getMinutes()) + ':' + twoDigit(msgDate.getSeconds())))
-    .append($('<span>').text(parsed.user + ': ').css('color',colors[parsed.userId]))
-    .append($('<span>').text(parsed.message)));
-
+function scrollChat() {
   if(($('.chat div').length - 1) * 30 - $('.chat').scrollTop() > $('.chat').height()){
     console.log('no scroll');
   }
@@ -46,12 +46,40 @@ function handleMessage(msg) {
   }
 }
 
+function addMessageToChat(parsed) {
+  var msgDate = new Date(parsed.timestamp);
+  $('.chat').append($('<div>')
+    .append($('<span>').addClass('chat-timestamp').text(msgDate.getHours() % 12 + ':' + twoDigit(msgDate.getMinutes()) + ':' + twoDigit(msgDate.getSeconds())))
+    .append($('<span>').text(parsed.user + ': ').css('color',colors[parsed.userId]))
+    .append($('<span>').text(parsed.message)));
+
+  scrollChat();
+}
+
+function handleDraftPick(parsed) {
+}
+
+function handleMessage(msg) { 
+  var parsed = JSON.parse(msg.data);
+
+  if(parsed.message){
+    addMessageToChat(parsed);
+  }
+  if(parsed.draft){
+    handleDraftPick(parsed);
+  }
+}
+
 $(document).ready(function(){
   $.getJSON('/data/players', function(players){
+    playerData = players;
     $('#player-search').autocomplete(_.map(players, playerText), {matchContains : true, max : 20});
     $('#player-search').result(function(){
       var newRow = $('#player-list tbody .template').clone().removeClass('template');
-      newRow.find('.name').text($(this).val());
+      newRow.find('.name').text($(this).val()).attr({target: '_blank', href : 'http://rivals.yahoo.com/ncaa/basketball/players/' + idFromName($(this).val())});
+      newRow.find('.icon-remove').click(function(){
+        newRow.remove();
+      });
       $('#player-list tbody').append(newRow);
       $(this).val('');
     });
@@ -68,4 +96,9 @@ $(document).ready(function(){
       $(this).val('');
     }
   });
+
+  setTimeout(function(){
+    send(ws, 'Chat Room');
+    $('#player-search').val('Trey Burke (Michigan Wolverines)').trigger('result');
+  }, 500);
 });
