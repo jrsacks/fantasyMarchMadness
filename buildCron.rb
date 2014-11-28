7aff95788c673562deb61ae3e4f7d0e6a692ec85
Jeff Sacks <jrsacks@gmail.com>
2014-11-28 17:51:40 -0600

    only finding conference games

--- buildCron.rb
+++ buildCron.rb
@@ -3,11 +3,28 @@
 require 'nokogiri'
 require 'open-uri'
 
-tv = ["CBS","TBS","TNT","TRU"]
+teams = [
+  "illinois-fighting-illini",
+  "indiana-hoosiers",
+  "iowa-hawkeyes",
+  "maryland-terrapins",
+  "michigan-state-spartans",
+  "michigan-wolverines",
+  "minnesota-golden-gophers",
+  "nebraska-cornhuskers",
+  "northwestern-wildcats",
+  "ohio-state-buckeyes",
+  "penn-state-nittany-lions",
+  "purdue-boilermakers",
+  "rutgers-scarlet-knights",
+  "wisconsin-badgers"
+]
+
 date = ARGV[0] || Time.now.to_s.split(' ').first
-doc = Nokogiri::HTML(open("http://sports.yahoo.com/college-basketball/scoreboard/?conf=all&date=#{date}"))
+doc = Nokogiri::HTML(open("http://sports.yahoo.com/college-basketball/scoreboard/?conf=3&date=#{date}"))
 doc.css(".game").each do |game|
-  if tv.include? game.css('.tv').text
+  conf_teams = teams.select { |t| game.attr("data-url").match(t) }
+  if conf_teams.length == 2
     time = game.css('.time').text
     gid = game.attr("data-url").gsub("/ncaab/","").gsub("/","")
     hour = 0
@@ -24,7 +41,6 @@ doc.css(".game").each do |game|
       hour = splitup.first.to_i - 1
     end
 
-    #if num tables > 8, divide by 2 for all cron
     puts "#{min}-59 #{hour} * * * curl localhost:4567/game/#{gid}"
     puts "* #{hour+1}-#{hour + 2} * * * curl localhost:4567/game/#{gid}"
     puts "0-#{min} #{(hour+3)%24} * * * curl localhost:4567/game/#{gid}"
