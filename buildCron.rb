ebeedd0307e9d8d5e55d45248efea6b2280bc933
Jeff Sacks <jrsacks@gmail.com>
2016-09-19 08:00:32 -0500

    updating build cron script for new API

--- buildCron.rb
+++ buildCron.rb
@@ -1,35 +1,24 @@
 #!/usr/bin/env ruby
 
-require 'nokogiri'
+require 'json'
+require 'date'
 require 'open-uri'
 
 tv = ["CBS","TBS","TNT","TRU"]
 date = ARGV[0] || Time.now.to_s.split(' ').first
-doc = Nokogiri::HTML(open("http://sports.yahoo.com/college-basketball/scoreboard/?conf=all&date=#{date}"))
-games = doc.css(".game")
-tv_games = games.select do |game|
-  tv.include? game.css('.tv').text
+data = JSON.parse(open("https://api-secure.sports.yahoo.com/v1/editorial/s/scoreboard?leagues=ncaab&date=#{date}").read)
+games = data["service"]["scoreboard"]["games"]
+tv_games = games.select do |key, game|
+  tv.include? game["tv_coverage"]
 end
 
-tv_games.each do |game|
-  time = game.css('.time').text
-  gid = game.attr("data-url").gsub("/ncaab/","").gsub("/","")
-  hour = 0
-  min = 0
-  splitup = time.split(' ').first.split(':')
-  min = splitup.last 
-  if time.match(/pm/i)
-    if splitup.first.to_i == 12
-      hour = 11 
-    else
-      hour = splitup.first.to_i + 11
-    end
-  else
-    hour = splitup.first.to_i - 1
-  end
+tv_games.each do |gid, game|
+  start_time = DateTime.parse(game["start_time"]).to_time
+  hour = start_time.hour
+  min = start_time.min
 
-  #if num tables > 8, divide by 2 for all cron
-  dividor = "/2" if tv_games.length > 8
+  #if games > 8, divide by 2 for all cron
+  dividor = "/2" if tv_games.keys.length > 8
   puts "#{min}-59#{dividor} #{hour} * * * curl localhost:4567/game/#{gid} #hts"
   puts "*#{dividor} #{hour+1}-#{hour + 2} * * * curl localhost:4567/game/#{gid} #hts"
   puts "0-#{min}#{dividor} #{(hour+3)%24} * * * curl localhost:4567/game/#{gid} #hts"
