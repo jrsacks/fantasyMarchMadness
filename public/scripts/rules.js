b601fa3473f41daf1734e519fb07742167eff7de
Jeff Sacks <jrsacks@gmail.com>
2018-12-22 10:44:18 -0600

    getting ready for 2019 season

--- public/scripts/rules.js
+++ public/scripts/rules.js
@@ -4,9 +4,9 @@ function sortedGameScores(players){
 
 function teamTotal(players){
   var year = historicYear();
-  if(currentYear()){
+  if(year === '2018'){
     return sortedGameScores(players).slice(0,128).sum().toFixed(1);
-  } else if(year === '2016' || year === '2017'){
+  } else if(currentYear() || year === '2016' || year === '2017'){
     return sortedGameScores(players).slice(0,144).sum().toFixed(1);
   } else if(year === '2015'){
     return _.pluck(players.slice(0,8), 'points').sum();
@@ -17,18 +17,22 @@ function projectedTeamInfo(total, players){
   var year = historicYear();
   var games = sortedGameScores(players);
   if(currentYear() && games.length > 0){
-    if(games.length < 128) {
+    var n = 144;
+    if(games.length < n) {
       var avg = (total / games.length).toFixed(1);
       return ' (' + games.length + ' - ' + avg + ')';
     } else {
-      var avg = (total / 128).toFixed(1);
-      return ' (' + avg + ' - ' + games.slice(0,128).min().toFixed(1) + ')';
+      var avg = (total / n).toFixed(1);
+      return ' (' + avg + ' - ' + games.slice(0,n).min().toFixed(1) + ')';
     }
-  }
-  if(year === '2016' || year === '2017'){
-    var min = (games.slice(0,144).min() || 0).toFixed(1);
-    var avg = (total / 144).toFixed(1);
-    if(games.length < 144){
+  } else {
+    var n = 144;
+    if(year === '2018'){
+      n = 128;
+    }
+    var min = (games.slice(0,n).min() || 0).toFixed(1);
+    var avg = (total / n).toFixed(1);
+    if(games.length < n){
       min = 0;
     }
     return ' (' + avg + ' - ' + min + ')';
@@ -36,42 +40,52 @@ function projectedTeamInfo(total, players){
   return '';
 }
 
+function multiplierFor2018(stats){
+  var multiplier = 1;
+
+  if(stats.winner){
+    multiplier *= 1.4
+  }
+  if(stats.fouls === 5){
+    multiplier *= 0.7;
+  }
+
+  var overTen = _.filter([stats.points, stats.rebounds, stats.steals, stats.assists, stats.blocks, stats.threes], s => s >= 10).length;
+
+  if(overTen > 2) {
+    multiplier *= 2;
+  } else if (overTen === 2){
+    multiplier *= 1.1
+  }
+
+  if(stats.fts_attempted >= 10 && stats.fts === stats.fts_attempted){
+    multiplier *= 1.2
+  }
+
+  if(stats.fts_attempted >= 5 && ((stats.fts / stats.fts_attempted) < 0.5)){
+    multiplier *= 0.7
+  }
+
+  if(stats.turnovers >= 10){
+    multiplier *= 0.5;
+  }
+
+  if(stats.threes_attempted >= 5 && stats.threes === stats.threes_attempted){
+    multiplier *= 1.5
+  }
+
+  return multiplier;
+}
+
+function multiplierFor2019(stats){
+  return multiplierFor2018(stats);
+}
+
 function multiplierForGame(stats){
   if(currentYear()){
-    var multiplier = 1;
-
-    if(stats.winner){
-      multiplier *= 1.4
-    }
-    if(stats.fouls === 5){
-      multiplier *= 0.7;
-    }
-
-    var overTen = _.filter([stats.points, stats.rebounds, stats.steals, stats.assists, stats.blocks, stats.threes], s => s >= 10).length;
-
-    if(overTen > 2) {
-      multiplier *= 2;
-    } else if (overTen === 2){
-      multiplier *= 1.1
-    }
-
-    if(stats.fts_attempted >= 10 && stats.fts === stats.fts_attempted){
-      multiplier *= 1.2
-    }
-    
-    if(stats.fts_attempted >= 5 && ((stats.fts / stats.fts_attempted) < 0.5)){
-      multiplier *= 0.7
-    }
-    
-    if(stats.turnovers >= 10){
-      multiplier *= 0.5;
-    }
-
-    if(stats.threes_attempted >= 5 && stats.threes === stats.threes_attempted){
-      multiplier *= 1.5
-    }
-
-    return multiplier;
+    return multiplierFor2019(stats);
+  } else if(historicYear() === '2018'){
+    return multiplierFor2018(stats);
   } else if(historicYear() === '2016' || historicYear() === '2017'){
     return stats.winner ? 1.4 : 1.0;
   } else {
@@ -99,7 +113,7 @@ function shouldAddGame(player, stats){
   if(player.waived || player.pickup){
     var waiveDate = "";
 
-    if(currentYear()){
+    if(historicYear() === "2018"){
       waiveDate = "20180129";
       if(player.team.match(/Nebraska/) || player.team.match(/Michigan/)){
         waiveDate = "20180126";
