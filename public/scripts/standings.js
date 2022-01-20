99def807d443d5f8b31318d34eb20d0d034f69a2
Jeff Sacks <jrsacks@gmail.com>
2022-01-19 20:00:26 -0600

    not slicing countable games

--- public/scripts/standings.js
+++ public/scripts/standings.js
@@ -235,7 +235,7 @@ function buildPlayer(player, index){
   }
   playerContainer.find('.player-round').text(round);
   var numberOfGames = gameTotals.length;
-  var countable = _.sortBy(gameTotals, g => -g).slice(0, gamesToCount).sum();
+  var countable = _.sortBy(gameTotals, g => -g).sum();
   var average = (countable / (gameTotals.length || 1)).toFixed(1);
   playerContainer.find('.player-total').text(countable.toFixed(1) + " (" + average.toFixed(1) + ")");
   var nameText = player.name + " (" + numberOfGames + ")";
